4b93c1aa5267fdd4ff9f190b67d841e559afed79
Nathan Dionne <nrdionne@gmail.com>
2017-01-06 16:56:18 -0500

    whats up

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -62,13 +62,13 @@ if (moment().utc().hour() < 8) {
 // This demo emits data over Pusher to external resources for display.
 // Typically the handle function would run the logic invocation directly.
 // ----------------------------------------------------------------------------------
-exports.handle = function handle(client) {
-  // emitClientOverPusher(client, () => {
-    exports.runLogicInvocation(client)
-  // })
-}
+// exports.handle = function handle(client) {
+//   // emitClientOverPusher(client, () => {
+//     exports.runLogicInvocation(client)
+//   // })
+// }
 
-exports.runLogicInvocation = function runLogicInvocation(client) {
+exports.handle = function runLogicInvocation(client) {
   const env = client.getEnvironment()
   // const imgixClient = new ImgixClient({
   //   host: env.imgix.host,
